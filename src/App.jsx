e46cdf8fb6ae1072e33636ec961948ecc46b8c97
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-25 14:54:45 +0200

    fixed App and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,60 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import DefaultLayout from "./layouts/DefaultLayout"
 import Home from "./pages/Home"
 import Posts from "./pages/Posts"
 import PostShow from "./pages/PostShow"
 import PostEdit from "./pages/PostEdit"
 import PostCreate from "./pages/PostCreate"
-import { GlobalProvider } from "./contexts/GlobalContext"
 import AuthPage from "./middlewares/AuthPage"
-import { AuthProvider } from "./contexts/AuthContext"
 import Login from "./pages/Login"
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalProvider>
-        <AuthProvider>
+    <Routes>
 
-          <Routes>
+      {/* Rotte pubbliche */}
+      <Route path="/" element={<DefaultLayout />}>
 
-            {/* Rotte pubbliche */}
-            <Route path="/" element={<DefaultLayout />}>
+        {/* Home */}
+        <Route index element={<Home />} />
 
-              {/* Home */}
-              <Route index element={<Home />} />
+        <Route path="posts" >
 
-              <Route path="posts" >
+          {/* Index */}
+          <Route index element={<Posts />} />
 
-                {/* Index */}
-                <Route index element={<Posts />} />
+          <Route path=":slug">
 
-                <Route path=":slug">
+            {/* Show */}
+            <Route index element={<PostShow />} />
 
-                  {/* Show */}
-                  <Route index element={<PostShow />} />
+          </Route>
 
-                </Route>
+        </Route>
 
-              </Route>
+        {/* Rotta Login */}
+        <Route path="login" element={<Login />} />
 
-              {/* Rotta Login */}
-              <Route path="login" element={<Login />} />
+      </Route >
 
-            </Route >
+      {/* Rotte private */}
+      <Route path="/" element={<AuthPage><DefaultLayout /></AuthPage>}>
+        <Route path="posts" >
 
-            {/* Rotte private */}
-            <Route path="/" element={<AuthPage><DefaultLayout /></AuthPage>}>
-              <Route path="posts" >
+          <Route path=":slug">
 
-                <Route path=":slug">
+            {/* Edit */}
+            <Route path="edit" element={<PostEdit />} />
 
-                  {/* Edit */}
-                  <Route path="edit" element={<PostEdit />} />
+          </Route>
 
-                </Route>
+          {/* Create */}
+          <Route path="create" element={<PostCreate />} />
 
-                {/* Create */}
-                <Route path="create" element={<PostCreate />} />
+        </Route>
+      </Route>
 
-              </Route>
-            </Route>
-
-          </Routes>
-
-        </AuthProvider>
-      </GlobalProvider>
-    </BrowserRouter>
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './scss/index.scss'
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalProvider } from './contexts/GlobalContext.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GlobalProvider>
+        <AuthProvider>
+
+          <App />
+
+        </AuthProvider>
+      </GlobalProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
